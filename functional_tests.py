cfefd7bd2231c13bbda8ea4b354865444c9f14b5
nantianqi <305858610@qq.com>
2015-10-22 10:09:27 +0800

    using unittest module

--- functional_tests.py
+++ functional_tests.py
@@ -1,18 +1,29 @@
 # -*- coding: utf-8 -*-
 
 from selenium import  webdriver
+import unittest
 
 
+def setProxy():
+    profile=webdriver.FirefoxProfile()
+    profile.set_preference("network.proxy.type",1)
+    profile.set_preference("network.proxy.http","135.245.192.6")
+    profile.set_preference("network.proxy.http_port",8000)
+    profile.update_preferences()
 
-profile=webdriver.FirefoxProfile()
-profile.set_preference("network.proxy.type",1)
-profile.set_preference("network.proxy.http","135.245.192.6")
-profile.set_preference("network.proxy.http_port",8000)
+    return profile
 
-profile.update_preferences()
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):  #
+        profile=setProxy()
+        self.browser = webdriver.Firefox(firefox_profile=profile)
+        self.browser.implicitly_wait(3)
+    def tearDown(self):
+        self.browser.quit()
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('Django', self.browser.title)  #
+        #self.fail('Finish the test!')
 
-browser=webdriver.Firefox(firefox_profile=profile)
-browser.get('http://127.0.0.1:8000')
-#browser.get('http://www.baidu.com')
-assert 'Django' in browser.title
-
+if __name__ == '__main__':  #
+    unittest.main(warnings='ignore')
