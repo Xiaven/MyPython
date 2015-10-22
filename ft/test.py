588b68580874069d07e52299d26a9511e7f830ac
nantianqi <305858610@qq.com>
2015-10-22 15:49:31 +0800

    use LiveServerTestCase to make ft an app

--- ft/test.py
+++ ft/test.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-
+from django.test import LiveServerTestCase
 from selenium import  webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
+#import unittest
 
 
 def setProxy():
@@ -14,7 +14,7 @@ def setProxy():
 
     return profile
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):  #
         profile=setProxy()
         self.browser = webdriver.Firefox(firefox_profile=profile)
@@ -30,7 +30,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
